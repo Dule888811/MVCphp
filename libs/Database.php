d96e83e1c09bf4a7c48f544906679c43762c5177
misko.vestica <misko.vestica@gmail.com>
2020-01-10 18:04:46 +0100

    little fix

--- libs/Database.php
+++ libs/Database.php
@@ -3,7 +3,7 @@ class Database extends PDO
 {
     public function __construct()
     {
-        parent::__construct('mysql:host=localhost;dbname=test', 'root','');
+        parent::__construct('mysql:host=localhost;dbname=mvc','root','');
         
         
     }
--- models/login_model.php
+++ models/login_model.php
@@ -3,21 +3,24 @@ class Login_Model extends Model
 {
     public function __construct()
     {
- 
-       
+        $this->db = new PDO('mysql:host=localhost;dbname=mvc','root','');
+        
     }
   
     public function run()
     {
         $uname = $_POST['uname'];
         $password = $_POST['psw'];
-        $this->db->prepare("SELECT id from users WHERE username = :username and password = :password");
-        $this->db->execute(array(
+     /*   $this->db->prepare("SELECT id from users WHERE username = :username and password = :password");
+        $this->db->exec(array(
             ':username' => $uname,
             ':password' => $password
-        ));
-        $data = fatchAll();
-        var_dump($data);
+        )); */
+        $stmtUserCheck = $this->db->prepare("SELECT * FROM mvc.users WHERE username=? AND password=?");
+		$stmtUserCheck->bindValue(1,$uname);
+        $stmtUserCheck->bindValue(2,$password);
+        $stmtUserCheck->execute();
+        var_dump($stmtUserCheck);
     }
   
 }
